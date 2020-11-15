672da17d4332ac218cdfc9ef0ae0bbfc01286579
Kosiro <kosiro.developer@gmail.com>
2020-11-15 22:08:11 +0100

    Subida inicial

---
+++ js/js.js
@@ -0,0 +1,18 @@
+'use strict'
+
+// var baños = document.querySelector("#value1");
+
+// document.getElementById("select2").addEventListener("click", function(){
+//     alert("Funciono");
+// });
+
+var div_cont = document.getElementById("contenedor-imagenes");
+
+
+// var select1 = document.createElement('select');
+// select1.setAttribute("id","select1");
+// select1.setAttribute("class","form-control");
+// var select2 = document.createElement('select');
+// select1.setAttribute("id","select2");
+// select1.setAttribute("class","form-control");
+// document.querySelector("#div_select").appendChild(select2);
